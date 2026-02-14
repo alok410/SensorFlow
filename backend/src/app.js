ef12d524bf056f8482a1820bef58e3b00b4d1110
Alok Patel <alokpatel41001@gmail.com>
2026-02-14 19:50:12 +0530

    sec

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
+// app.use('/api/auth', authRoutes);
 app.use('/api/sec', authRoutes);
 app.use("/api/locations", locationRoutes);
 
