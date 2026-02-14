7f13ebff949b7695271c6a4d05f5d0d48c751f8b
Alok Patel <alokpatel41001@gmail.com>
2026-02-14 19:45:06 +0530

    sec

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 
 import authRoutes from './routes/auth.routes.js';
 import locationRoutes from './routes/location.routes.js'
-import secretaryRoutes from "./routes/secretary.routes.js";
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use("/api/locations", locationRoutes);
-app.use("/api/secretaries", secretaryRoutes);
 
 
 // Health check
