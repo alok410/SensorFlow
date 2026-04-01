e65fa2768aa4fb200fcbe893210799689389c95d
Alok Patel <alokpatel41001@gmail.com>
2026-04-01 14:05:05 +0530

    main meter added

--- backend/src/routes/meter.routes.js
+++ backend/src/routes/meter.routes.js
@@ -1,9 +1,9 @@
 // routes/meter.routes.js
 import express from "express";
-import { fetchAndStoreMeterData } from "../controllers/meter.controller.js";
+import { storeMeterData } from "../controllers/meter.controller.js";
 
 const router = express.Router();
 
-router.get("/fetch-meter-data", fetchAndStoreMeterData);
+router.get("/fetch-meter-data", storeMeterData);
 
 export default router;
